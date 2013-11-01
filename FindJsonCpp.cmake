12f368cb7ee7d23a5ea8695e0e7bc76a974b9133
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2013-11-01 20:15:29 +0100

    lol

--- FindJsonCpp.cmake
+++ FindJsonCpp.cmake
@@ -1,11 +1,26 @@
-find_path(JSONCPP_INCLUDE_DIR json/json.h
-          PATH_SUFFIXES jsoncpp)
-find_library(JSONCPP_LIBRARY jsoncpp)
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules (JSONCPP jsoncpp)
+endif()
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(JsonCpp DEFAULT_MSG JSONCPP_LIBRARY JSONCPP_INCLUDE_DIR)
+if(NOT JSONCPP_FOUND)
+  find_path(JSONCPP_INCLUDE_DIR json/json.h
+            PATH_SUFFIXES jsoncpp)
+  find_library(JSONCPP_LIBRARY jsoncpp)
+endif()
 
-MARK_AS_ADVANCED(
-  JSONCPP_INCLUDE_DIR
-  JSONCPP_LIBRARY
-)
+if(NOT JSONCPP_FOUND)
+  message(STATUS "Building JsonCpp")
+  include(ExternalProject)
+  externalproject_add(JsonCpp
+                      URL http://garr.dl.sourceforge.net/project/jsoncpp/jsoncpp/0.5.0/jsoncpp-src-0.5.0.tar.gz
+                      PREFIX JsonCpp
+                      PATCH_COMMAND patch -p0 < ${PROJECT_SOURCE_DIR}/01-jsoncpp-add-cmake-buildsystem.patch
+                      CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>)
+  set(JSONCPP_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/JsonCpp/include/jsoncpp)
+  set(JSONCPP_LIBRARIES ${CMAKE_BINARY_DIR}/JsonCpp/lib/libjsoncpp.a)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(JsonCpp DEFAULT_MSG JSONCPP_LIBRARIES JSONCPP_INCLUDE_DIRS)
+
+mark_as_advanced(JSONCPP_INCLUDE_DIRS JSONCPP_LIBRARIES)
